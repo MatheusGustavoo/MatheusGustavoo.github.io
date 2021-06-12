0b0a516fa5d26e08264a65c15ddc241a92071697
mbrplay <56611686+mbrplay@users.noreply.github.com>
2021-06-12 14:02:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-function executar1(){
-const imagens = document.querySelectorAll('.js-div li')
-const frase = document.querySelectorAll('.descr-1 img')
-frase[0].classList.add('ativo')
-
-function navTab(index){
-  frase.forEach((remover) =>{
-    remover.classList.remove('ativo')
-  });
-  frase[index].classList.add('ativo');
-}
-
-imagens.forEach((img, index) =>{
-  img.addEventListener('click', function(){
-    navTab(index)
-  })
-})
-
-console.log(imagens.index)
-}
+//  function executar1(){
+//   const imagens = document.querySelectorAll('.js-div li')
+//   const frase = document.querySelectorAll('.descr-1 img')
+//   frase[0].classList.add('ativo')
+  
+//   function navTab(index){
+//     frase.forEach((remover) =>{
+//       remover.classList.remove('ativo')
+//     });
+//     frase[index].classList.add('ativo');
+//   }
+  
+//   imagens.forEach((img, index) =>{
+//     img.addEventListener('click', function(){
+//       navTab(index)
+//     })
+//   })
+  
+//   console.log(imagens.index)
+//   }
+//   executar1();
+import executar1 from './slide.js'
 executar1();
-
 
 function executar2(){
 const linkIN =  document.querySelectorAll('.option a')
@@ -63,3 +64,27 @@ function animaScroll() {
 }
 
 window.addEventListener('scroll', animaScroll);  
+
+
+
+function Carro(Preco, Marca){
+  this.preco = Preco;
+  this.marca = Marca;
+}
+
+
+const honda = new Carro(2000, 'honda');
+
+
+const fiat = new Carro(2500, 'fiat');
+
+function Carro2(marca, precoInicial){
+  const taxa = 1.2;
+  const precofinal = precoInicial * taxa;
+ 
+  this.marca = marca;
+  this.preco = precofinal;
+
+}
+
+const celta = new Carro2('celta', 1000);
